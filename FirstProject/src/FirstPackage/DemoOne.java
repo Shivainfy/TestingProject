e1486fb19ec1ef7df79451fc291e3769c9642310
WIN <WIN@DESKTOP-E7HV5C9>
2019-10-19 16:08:15 +0530

    FirstOne cloned

---
+++ FirstProject/src/FirstPackage/DemoOne.java
@@ -0,0 +1,43 @@
+package FirstPackage;
+
+public class DemoOne
+{
+	int num;
+	String town;
+	
+	public void display()
+	{
+		System.out.println("Learning");
+	}
+	public void displeyTwo()
+	{
+		System.out.println("enter the new dragon");
+	}
+	public void Printtwo(String name, int age)
+	{
+		System.out.println("learning something");
+	}
+	public static void king()
+	{
+		System.out.println("smartphone");
+	}
+	
+	public void printStar(String name, int age, double marks)
+	{
+		System.out.println("story");
+	}
+	
+	public int LearningNew(int a, int b)
+	{
+		int c = a+b;
+		System.out.println(c);
+		return c;
+		//System.out.println(c);
+	}
+	
+	public DemoOne(int num, String town)
+	{
+		this.num = num;
+		this.town = town;
+	}
+}
---
+++ FirstProject/src/FirstPackage/TestSample.java
@@ -0,0 +1,18 @@
+package FirstPackage;
+
+public class TestSample 
+{
+	public static void main(String[] args)
+	{
+		// TODO Auto-generated method stub
+		System.out.println("testing");
+		DemoOne de = new DemoOne(45, "bangalore");
+		de.display();
+		de.displeyTwo();
+		de.Printtwo("shiva", 56);
+		//de.king();
+		DemoOne.king();
+		de.printStar("RAJ", 80, 45.8);
+		de.LearningNew(20, 50);
+	}
+}
